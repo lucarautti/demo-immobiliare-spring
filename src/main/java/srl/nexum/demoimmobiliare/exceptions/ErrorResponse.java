1e4a1a6c8fceab68c88768db54e95f15bcb6536b
Luca Rautti <l.rautti@nexum.srl>
2020-06-25 18:10:29 +0200

    Fix set/get ErrorResponse

--- src/main/java/srl/nexum/demoimmobiliare/exceptions/ErrorResponse.java
+++ src/main/java/srl/nexum/demoimmobiliare/exceptions/ErrorResponse.java
@@ -1,12 +1,22 @@
 package srl.nexum.demoimmobiliare.exceptions;
 
-import lombok.Data;
-
 import java.util.Date;
 
-@Data
 public class ErrorResponse {
 	private Date data = new Date();
 	private int codice;
 	private String messaggio;
+
+	public void setData(Date data) {this.data = data;}
+	public void setCodice(int codice) {this.codice = codice;}
+	public void setMessaggio(String messaggio) {this.messaggio = messaggio;}
+
+	public Date getData(){return data;}
+	public int getCodice(){return codice;}
+	public String getMessaggio(){return messaggio;}
+
+	@Override
+	public String toString(){
+		return "Exception [data=" +data+", codice="+codice+", messaggio="+messaggio+"]";
+	}
 }
